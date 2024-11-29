e0fa910737d115aaebad63964d36d12ac235b636
Sudalai Madan <62635527+Sudalai29@users.noreply.github.com>
2024-11-29 19:07:55 +0530

    Update StartApplication.java

--- javaproject/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ javaproject/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -1,16 +1,16 @@
 package com.abhishek;
-adding errora
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-SpringBootApplication
-Controller
+@SpringBootApplication
+@Controller
 public class StartApplication {
 
-    GetMapping("/")
+    @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "I have successfully built a Spring Boot application using Maven");
         model.addAttribute("msg", "This application is deployed onto Kubernetes using Argo CD");
