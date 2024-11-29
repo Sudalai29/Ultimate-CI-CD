5903d6b50323e5b34c005ff006c7f6b6c15fdda2
Sudalai Madan <62635527+Sudalai29@users.noreply.github.com>
2024-11-29 18:44:42 +0530

    Update StartApplication.java

--- javaproject/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ javaproject/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -1,12 +1,12 @@
 package com.abhishek;
-
+adding errora
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
+SpringBootApplication
 @Controller
 public class StartApplication {
 
